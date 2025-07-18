6d8bddde8a30ddea7b9970c687faa5e65d19003e
THOWFIQ <sthowfiq@gmail.com>
2025-07-18 22:18:03 +0530

    Update bug.py

--- bug.py
+++ bug.py
@@ -1,1 +1,8 @@
-Error processing 1004543337: 'NoneType' object has no attribute 'get'
+# Fetch Sales Order
+salesorder_resp = post_api(FID, fetch_salesorder_query(salesorder_id))
+get_by_salesorderids = salesorder_resp['data'].get('getBySalesorderids')
+if not get_by_salesorderids or not get_by_salesorderids.get('result'):
+    raise ValueError(f"No result found for Sales Order ID: {salesorder_id}")
+
+result = get_by_salesorderids['result'][0]
+combined_data['data']['getBySalesorderids'] = get_by_salesorderids
