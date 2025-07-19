43f6f59811abde0dfa432abab918f041d098c998
THOWFIQ <sthowfiq@gmail.com>
2025-07-19 07:51:57 +0530

    Update new_graph_ql.py

--- new_graph_ql.py
+++ new_graph_ql.py
@@ -1,22 +1,41 @@
-# graphqlQueries.py
-
-# --------------------- GraphQL Queries ---------------------
+import os
+import sys
+import json
 
 def fetch_soaorder_query():
     return """
     query MyQuery($salesorderIds: [String!]!) {
-      getSoheaderBySoids(salesOrderIds: $salesorderIds) {
+    getSoheaderBySoids(salesOrderIds: $salesorderIds) {
         buid
         ppDate
         orderDate
-      }
+    }
     }
     """
 
-def fetch_salesorder_query(salesorderId):
+def fetch_fulfillment_query():
+    return """
+    query MyQuery($fulfillment_id: String!) {
+    getFulfillmentsById(fulfillmentId: $fulfillment_id) {
+        soHeaderRef
+        buid
+        salesOrderId
+        region
+        fulfillments {
+        systemQty
+        shipByDate
+        updateDate
+        salesOrderLines {
+            lob
+        }
+        }
+    }
+    }
+    """
+def fetch_salesorder_query(salesorderIds):
     return f"""
     query MyQuery {{
-      getBySalesorderids(salesorderIds: "{salesorderId}") {{
+      getBySalesorderids(salesorderIds: "{salesorderIds}") {{
         result {{
           fulfillment {{
             fulfillmentId
@@ -43,72 +62,52 @@ def fetch_salesorder_query(salesorderId):
 def fetch_workOrderId_query(workOrderId):
     return f"""
     query MyQuery {{
-      getWorkOrderById(workOrderId: "{workOrderId}") {{
+        getWorkOrderById(workOrderId: "{workOrderId}") {{
         channelStatusCode
         woLines {{
-          ismultipack
+            ismultipack
         }}
         shipMode
         updateDate
         isOtmEnabled
         woId
         shipToFacility
-      }}
+        }}
     }}
     """
 
 def fetch_getByWorkorderids_query(workOrderId):
     return f"""
     query MyQuery {{
-      getByWorkorderids(workorderIds: "{workOrderId}") {{
+        getByWorkorderids(workorderIds: "{workOrderId}") {{
         result {{
-          asnNumbers {{
+            asnNumbers {{
             snNumber
-          }}
-          fulfillment {{
+            }}
+            fulfillment {{
             fulfillmentId
-          }}
-          salesOrder {{
+            }}
+            salesOrder{{
             salesOrderId
-          }}
-          fulfillmentOrders {{
+            }},
+            fulfillmentOrders{{
             foId
-          }}
-          workOrder {{
+            }}
+            workOrder {{
             woId
-          }}
+            }}
         }}
-      }}
+        }}
     }}
-    """
-
-def fetch_fulfillment_query():
-    return """
-    query MyQuery($fulfillment_id: String!) {
-      getFulfillmentsById(fulfillmentId: $fulfillment_id) {
-        soHeaderRef
-        buid
-        salesOrderId
-        region
-        fulfillments {
-          systemQty
-          shipByDate
-          updateDate
-          salesOrderLines {
-            lob
-          }
-        }
-      }
-    }
     """
 
 def fetch_getFulfillmentsBysofulfillmentid_query(fulfillment_id):
     return f"""
     query MyQuery {{
-      getFulfillmentsBysofulfillmentid(fulfillmentId: "{fulfillment_id}") {{
+        getFulfillmentsBysofulfillmentid(fulfillmentId: "{fulfillment_id}") {{
         fulfillments {{
-          shipByDate
-          address {{
+            shipByDate
+            address {{
             taxRegstrnNum
             addressLine1
             postalCode
@@ -118,53 +117,51 @@ def fetch_getFulfillmentsBysofulfillmentid_query(fulfillment_id):
             customerNameExt
             country
             createDate
-          }}
-          oicId
-          shipCode
-          mustArriveByDate
-          updateDate
-          mergeType
-          manifestDate
-          revisedDeliveryDate
-          deliveryCity
+            }}
+            oicId
+            shipCode
+            mustArriveByDate
+            updateDate
+            mergeType
+            manifestDate
+            revisedDeliveryDate
+            deliveryCity
         }}
         sourceSystemId
         salesOrderId
-      }}
+        }}
     }}
     """
 
 def fetch_getAllFulfillmentHeadersSoidFulfillmentid_query(fulfillment_id):
     return f"""
     query MyQuery {{
-      getAllFulfillmentHeadersSoidFulfillmentid(fulfillmentId: "{fulfillment_id}") {{
+        getAllFulfillmentHeadersSoidFulfillmentid(fulfillmentId: "{fulfillment_id}") {{
         isDirectShip
-      }}
+        }}
     }}
     """
 
 def fetch_getFbomBySoFulfillmentid_query(fulfillment_id):
     return f"""
     query MyQuery {{
-      getFbomBySoFulfillmentid(fulfillmentId: "{fulfillment_id}") {{
+        getFbomBySoFulfillmentid(fulfillmentId: "{fulfillment_id}") {{
         ssc
-      }}
+        }}
     }}
     """
-
 def fetch_foid_query(fo_id):
     return f"""
     query MyQuery {{
-      getAllFulfillmentHeadersByFoId(foId: "{fo_id}") {{
+    getAllFulfillmentHeadersByFoId(foId: "{fo_id}") {{
         foId
         forderline {{
-          shipFromFacility
-          shipToFacility
+        shipFromFacility
+        shipToFacility
         }}
-      }}
+    }}
     }}
     """
-
 def fetch_getByFulfillmentids_query(fulfillmentid):
     return f"""
     query MyQuery {{
@@ -190,72 +187,93 @@ def fetch_getByFulfillmentids_query(fulfillmentid):
     }}
     """
 
-def fetch_getOrderDate_query(orderFromDate, orderToDate):
-    return f"""
-    query MyQuery {{
-      getOrdersByDate(fromDate: "{orderFromDate}", toDate: "{orderToDate}") {{
-        result {{
-          fulfillmentId
-          oicId
-          mustArriveByDate
-          salesOrderId
-          deliveryCity
-          manifestDate
-          shipByDate
-          shipCode
-          updateDate
-          soHeaderRef
-          systemQty
-        }}
-      }}
-    }}
-    """
-
-
-# --------------------- Table Structure Builder ---------------------
-
-def tablestructural(data, IsPrimary):
+def tablestructural(data,IsPrimary):
     table_structure = {
-        "columns": [
-            {"value": "BUID", "sortBy": "ascending", "isPrimary": IsPrimary in ["APJ", "EMEA", "DAO"], "group": "ID", "checked": True},
-            {"value": "PP Date", "sortBy": "ascending", "isPrimary": True, "group": "Date", "checked": True},
-            {"value": "Sales Order Id", "sortBy": "ascending", "isPrimary": True, "group": "ID", "checked": True},
-            {"value": "Fulfillment Id", "sortBy": "ascending", "isPrimary": True, "group": "ID", "checked": True},
-            {"value": "Region Code", "sortBy": "ascending", "group": "Code", "checked": False},
-            {"value": "FoId", "sortBy": "ascending", "group": "ID", "checked": False},
-            {"value": "System Qty", "sortBy": "ascending", "isPrimary": IsPrimary in ["APJ", "EMEA"], "group": "Other", "checked": IsPrimary in ["APJ", "EMEA"]},
-            {"value": "Ship By Date", "sortBy": "ascending", "group": "Date", "checked": True},
-            {"value": "LOB", "sortBy": "ascending", "group": "Other", "checked": IsPrimary in ["APJ", "EMEA"]},
-            {"value": "Ship From Facility", "sortBy": "ascending", "group": "Facility", "checked": IsPrimary == "EMEA"},
-            {"value": "Ship To Facility", "sortBy": "ascending", "group": "Facility", "checked": IsPrimary == "EMEA"},
-            {"value": "Tax Regstrn Num", "sortBy": "ascending", "group": "Other", "checked": False},
-            {"value": "Address Line1", "sortBy": "ascending", "group": "Address", "checked": False},
-            {"value": "Postal Code", "sortBy": "ascending", "group": "Address", "checked": False},
-            {"value": "State Code", "sortBy": "ascending", "group": "Code", "checked": False},
-            {"value": "City Code", "sortBy": "ascending", "group": "Address", "checked": False},
-            {"value": "Customer Num", "sortBy": "ascending", "group": "Other", "checked": False},
-            {"value": "Customer Name Ext", "sortBy": "ascending", "group": "Other", "checked": False},
-            {"value": "Country", "sortBy": "ascending", "group": "Address", "checked": IsPrimary == "APJ"},
-            {"value": "Create Date", "sortBy": "ascending", "group": "Date", "checked": False},
-            {"value": "Ship Code", "sortBy": "ascending", "group": "Code", "checked": False},
-            {"value": "Must Arrive By Date", "sortBy": "ascending", "group": "Date", "checked": False},
-            {"value": "Update Date", "sortBy": "ascending", "group": "Date", "checked": False},
-            {"value": "Merge Type", "sortBy": "ascending", "group": "Type", "checked": False},
-            {"value": "Manifest Date", "sortBy": "ascending", "group": "Date", "checked": False},
-            {"value": "Revised Delivery Date", "sortBy": "ascending", "group": "Date", "checked": False},
-            {"value": "Delivery City", "sortBy": "ascending", "group": "Address", "checked": False},
-            {"value": "Source System Id", "sortBy": "ascending", "group": "ID", "checked": False},
-            {"value": "Is Direct Ship", "sortBy": "ascending", "group": "Flag", "checked": False},
-            {"value": "SSC", "sortBy": "ascending", "group": "Other", "checked": False},
-            {"value": "Vendor Work Order Num", "sortBy": "ascending", "group": "ID", "checked": False},
-            {"value": "Channel Status Code", "sortBy": "ascending", "group": "Code", "checked": False},
-            {"value": "Ismultipack", "sortBy": "ascending", "group": "Flag", "checked": False},
-            {"value": "Ship Mode", "sortBy": "ascending", "group": "Mode", "checked": False},
-            {"value": "Is Otm Enabled", "sortBy": "ascending", "group": "Flag", "checked": False},
-            {"value": "SN Number", "sortBy": "ascending", "group": "ID", "checked": False},
-            {"value": "OIC ID", "sortBy": "ascending", "group": "ID", "checked": False},
-            {"value": "Order Date", "sortBy": "ascending", "group": "Date", "checked": IsPrimary in ["APJ", "DAO"]}
+        "columns": [            
+            {"value": "BUID", "sortBy": "ascending", "isPrimary": IsPrimary in ["APJ","EMEA","DAO"], "group": "ID", "checked": IsPrimary in ["APJ","EMEA","DAO"]},
+            {"value": "PP Date", "sortBy": "ascending", "isPrimary": IsPrimary in ["APJ","EMEA","DAO"], "group": "Date", "checked": IsPrimary in ["APJ","EMEA","DAO"]},
+            {"value": "Sales Order Id", "sortBy": "ascending", "isPrimary": IsPrimary in ["APJ","EMEA","DAO"], "group": "ID", "checked": IsPrimary in ["APJ","EMEA","DAO"]},
+            {"value": "Fulfillment Id", "sortBy": "ascending", "isPrimary": IsPrimary in ["APJ","EMEA","DAO"], "group": "ID", "checked": IsPrimary in ["APJ","EMEA","DAO"]},
+            {"value": "Region Code", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Code", "checked": IsPrimary in []},
+            {"value": "FoId", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "ID", "checked": IsPrimary in []},
+            {"value": "System Qty", "sortBy": "ascending", "isPrimary": IsPrimary in ["APJ","EMEA"], "group": "Other", "checked": IsPrimary in ["APJ","EMEA"]},
+            {"value": "Ship By Date", "sortBy": "ascending", "isPrimary": IsPrimary in ["APJ","EMEA","DAO"], "group": "Date", "checked": IsPrimary in ["APJ","EMEA","DAO"]},
+            {"value": "LOB", "sortBy": "ascending", "isPrimary": IsPrimary in ["APJ","EMEA"], "group": "Other", "checked": IsPrimary in ["APJ","EMEA"]},
+            {"value": "Ship From Facility", "sortBy": "ascending", "isPrimary": IsPrimary in ["EMEA"], "group": "Facility", "checked": IsPrimary in ["EMEA"]},
+            {"value": "Ship To Facility", "sortBy": "ascending", "isPrimary": IsPrimary in ["EMEA"], "group": "Facility", "checked": IsPrimary in ["EMEA"]},
+            {"value": "Tax Regstrn Num", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Other", "checked": IsPrimary in []},
+            {"value": "Address Line1", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Address", "checked": IsPrimary in []},
+            {"value": "Postal Code", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Address", "checked": IsPrimary in []},
+            {"value": "State Code", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Code", "checked": IsPrimary in []},
+            {"value": "City Code", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Address", "checked": IsPrimary in []},
+            {"value": "Customer Num", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Other", "checked": IsPrimary in []},
+            {"value": "Customer Name Ext", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Other", "checked": IsPrimary in []},
+            {"value": "Country", "sortBy": "ascending", "isPrimary": IsPrimary in ['APJ'], "group": "Address", "checked": IsPrimary in ['APJ']},
+            {"value": "Create Date", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Date", "checked": IsPrimary in []},
+            {"value": "Ship Code", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Code", "checked": IsPrimary in []},
+            {"value": "Must Arrive By Date", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Date", "checked": IsPrimary in []},
+            {"value": "Update Date", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Date", "checked": IsPrimary in []},
+            {"value": "Merge Type", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Type", "checked": IsPrimary in []},
+            {"value": "Manifest Date", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Date", "checked": IsPrimary in []},
+            {"value": "Revised Delivery Date", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Date", "checked": IsPrimary in []},
+            {"value": "Delivery City", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Address", "checked": IsPrimary in []},
+            {"value": "Source System Id", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "ID", "checked": IsPrimary in []},
+            {"value": "Is Direct Ship", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Flag", "checked": IsPrimary in []},
+            {"value": "SSC", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Other", "checked": IsPrimary in []},
+            {"value": "Vendor Work Order Num", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "ID", "checked": IsPrimary in []},
+            {"value": "Channel Status Code", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Code", "checked": IsPrimary in []},
+            {"value": "Ismultipack", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Flag", "checked": IsPrimary in []},
+            {"value": "Ship Mode", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Mode", "checked": IsPrimary in []},
+            {"value": "Is Otm Enabled", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Flag", "checked": IsPrimary in []},
+            {"value": "SN Number", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "ID", "checked": IsPrimary in []},
+            {"value": "OIC ID", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "ID", "checked": IsPrimary in []},
+            {"value":"Order Date","sortBy":"ascending","isPrimary":IsPrimary in ["APJ","DAO"],"group":"Date","checked":IsPrimary in ["APJ","DAO"]}
         ],
-        "data": data
+        "data": []
     }
+    table_structure["data"].extend(data)
     return table_structure
+
+def get_path(region, path, configPath):
+    region = region.upper()
+    path = path.upper()
+
+    if path == "FID":
+        if region == "DAO":
+            return configPath.get('Linkage_DAO')
+        elif region == "APJ":
+            return configPath.get('Linkage_APJ')
+        elif region == "EMEA":
+            return configPath.get('Linkage_EMEA')
+
+    elif path == "FOID":
+        if region == "DAO":
+            return configPath.get('FM_Order_DAO')
+        elif region in ("EMEA", "APJ"):
+            return configPath.get('FM_Order_EMEA_APJ')
+
+    elif path == "SOPATH":
+        if region == "DAO":
+            return configPath.get('SO_Header_DAO')
+        elif region in ("EMEA", "APJ"):
+            return configPath.get('SO_Header_EMEA_APJ')
+
+    elif path == "WOID":
+        if region == "DAO":
+            return configPath.get('WO_Details_DAO')
+        elif region in ("EMEA", "APJ"):
+            return configPath.get('WO_Details_EMEA_APJ')
+
+    elif path == "FFBOM":
+        if region == "DAO":
+            return configPath.get('FM_BOM_DAO')
+        elif region in ("EMEA", "APJ"):
+            return configPath.get('FM_BOM_EMEA_APJ')
+
+    return None
+
+def load_config():
+    # current_dir = os.path.dirname(os.path.abspath(__file__))
+    config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'config', 'config_ge4.json'))
+    with open(config_path, "r") as f:
+        return json.load(f)
