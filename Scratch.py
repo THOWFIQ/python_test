412b5b2bf42bb9a7d60567199f38f53178668863
THOWFIQ <sthowfiq@gmail.com>
2025-07-20 00:43:29 +0530

    Update Scratch.py

--- Scratch.py
+++ Scratch.py
@@ -95,23 +95,55 @@ def fileldValidation(filters, format_type, region):
     # Step 4: Threaded API calls for each supported field
     if 'Sales_Order_id' in primary_filters:
         so_ids = list(set(x.strip() for x in primary_filters['Sales_Order_id'].split(',') if x.strip()))
-        result_map['Sales_Order_id'] = threaded_fetch(fetch_soaorder_query, "salesorderIds", so_ids, SOPATH)
+        result_map['Sales_Order_id'] = threaded_fetch(fetch_salesorder_query, "salesorderIds", so_ids, SOPATH)
 
     if 'foid' in primary_filters:
         foids = list(set(x.strip() for x in primary_filters['foid'].split(',') if x.strip()))
-        result_map['foid'] = threaded_fetch(fetch_fm_order_query, "foids", foids, FOID)
+        result_map['foid'] = threaded_fetch(fetch_foid_query, "foids", foids, FOID)
 
     if 'wo_id' in primary_filters:
         woids = list(set(x.strip() for x in primary_filters['wo_id'].split(',') if x.strip()))
-        result_map['wo_id'] = threaded_fetch(fetch_wo_details_query, "woIds", woids, WOID)
+        result_map['wo_id'] = threaded_fetch(fetch_workOrderId_query, "woIds", woids, WOID)
+        here i need to call 
+        fetch_workOrderId_query
+        fetch_getByWorkorderids_query
 
     if 'Fullfillment Id' in primary_filters:
         ffs = list(set(x.strip() for x in primary_filters['Fullfillment Id'].split(',') if x.strip()))
+
+        ( here i need to call 
+        
+        combined_data = {'data': {}}
+    variables = {"fulfillment_id": fulfillment_id}
+
+    fulfillment_query = fetch_fulfillment_query()
+    fulfillment_data = post_api(URL=SOPATH, query=fulfillment_query, variables=variables)
+    if fulfillment_data and fulfillment_data.get('data'):
+        combined_data['data']['getFulfillmentsById'] = fulfillment_data['data']['getFulfillmentsById']
+
+    sofulfillment_query = fetch_getFulfillmentsBysofulfillmentid_query(fulfillment_id)
+    sofulfillment_data = post_api(URL=SOPATH, query=sofulfillment_query, variables=None)
+    if sofulfillment_data and sofulfillment_data.get('data'):
+        combined_data['data']['getFulfillmentsBysofulfillmentid'] = sofulfillment_data['data']['getFulfillmentsBysofulfillmentid']
+
+    directship_query = fetch_getAllFulfillmentHeadersSoidFulfillmentid_query(fulfillment_id)
+    directship_data = post_api(URL=FOID, query=directship_query, variables=None)
+    if directship_data and directship_data.get('data'):
+        combined_data['data']['getAllFulfillmentHeadersSoidFulfillmentid'] = directship_data['data']['getAllFulfillmentHeadersSoidFulfillmentid']
+
+    fbom_query = fetch_getFbomBySoFulfillmentid_query(fulfillment_id)
+    fbom_data = post_api(URL=FFBOM, query=fbom_query, variables=None)
+    if fbom_data and fbom_data.get('data'):
+        combined_data['data']['getFbomBySoFulfillmentid'] = fbom_data['data']['getFbomBySoFulfillmentid']
+
+    return combined_data
+    
+    )
         result_map['Fullfillment Id'] = threaded_fetch(fetch_fm_bom_query, "ffIds", ffs, FFBOM)
 
-    if 'order_date' in primary_filters:
-        dates = list(set(x.strip() for x in primary_filters['order_date'].split(',') if x.strip()))
-        result_map['order_date'] = threaded_fetch(fetch_order_date_query, "orderDates", dates, SOPATH)
+    # if 'order_date' in primary_filters:
+    #     dates = list(set(x.strip() for x in primary_filters['order_date'].split(',') if x.strip()))
+    #     result_map['order_date'] = threaded_fetch(fetch_getOrderDate_query, "orderDates", dates, SOPATH)
 
     # Debug: Print all results
     for key, res in result_map.items():
