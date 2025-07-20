75f2383ea13d287889b04beb3f07bbdf335ae1e9
THOWFIQ <sthowfiq@gmail.com>
2025-07-20 19:46:43 +0530

    Create sample_data_save.py

---
+++ sample_data_save.py
@@ -0,0 +1,66 @@
+{
+
+    "id": "412341234",
+
+    "region": "EMEA",
+
+    "format_type": "grid",
+
+	userName: "Amit_Chandra"
+
+	userEmail: "Amit_Chandra@dellteam.com"
+
+    "filters": {
+
+      "Sales_Order_id": "1004543337,1004543337",
+
+      "foid": "FO999999,1004543337",
+
+      "Fullfillment Id": "262135,262136",
+
+      "wo_id": "7360928459,7360928460",
+
+      "Sales_order_ref": "REF123456",
+
+      "Order_create_date": "2025-07-15",
+
+      "ISMULTIPACK": "Yes",
+
+      "BUID": "202",
+
+      "Facility": "WH_BANGALORE",
+
+      "Manifest_ID": "MANI0001",
+
+      "order_date": "2025-07-15"
+
+    },
+
+    "templatename": "asdf",
+
+    "workorderid": "787",
+
+    "shared": true,
+
+    "sharedUserName": "asdf",
+
+    "columns": [
+
+      {
+
+        "checked": true,
+
+        "group": "ID",
+
+        "isPrimary": false,
+
+        "sortBy": "ascending",
+
+        "value": "FoId"
+
+      }
+
+    ]
+
+  }
+ 
