05bcc5e0f75b7c1e7df0debc0475eedaccbe32d6
THOWFIQ <sthowfiq@gmail.com>
2025-07-24 00:10:58 +0530

    Update bug.py

--- bug.py
+++ bug.py
@@ -196,3 +196,37 @@ def delete_template(template_id):
         connection.close()
 
     return jsonify({'message': f'Template with TEMPLATE_ID {template_id} deleted successfully'})
+
+{
+    "template_id": "23478254",
+    "region": "DAO",
+    "format_type": "Export",
+	"userName": "Amit_Chandra",
+	"userEmail": "dellteams@dellteam.com",
+    "filters": {
+      "Sales_Order_id": "1004543337,1004543337",
+      "foid": "FO999999,1004543337",
+      "Fullfillment Id": "262135,262136",
+      "wo_id": "7360928459,7360928460",
+      "Sales_order_ref": "REF123456",
+      "Order_create_date": "2025-07-15",
+      "ISMULTIPACK": "Yes",
+      "BUID": "202",
+      "Facility": "WH_BANGALORE",
+      "Manifest_ID": "MANI0001",
+      "order_date": "2025-07-15"
+    },
+    "templatename": "asdf",
+    "workorderid": "787",
+    "shared": true,
+    "sharedUserName": "asdf",
+    "columns": [
+      {
+        "checked": true,
+        "group": "ID",
+        "isPrimary": false,
+        "sortBy": "ascending",
+        "value": "FoId"
+      }
+    ]
+  }
