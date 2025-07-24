e3b544a98453ddcf4f4afa654320418b4946d654
THOWFIQ <sthowfiq@gmail.com>
2025-07-25 00:47:36 +0530

    Update bug.py

--- bug.py
+++ bug.py
@@ -1,1 +1,11 @@
-[ERROR] Fetch failed for batch ['417960', '456702', '419006', '8040034769', '4475328993', '420976', '432545', '495089', '425003', '418390']: object dict can't be used in 'await' expression
+Total Sales Order IDs: 1
+{
+  "Sales_Order_id": [
+    {
+      "data": {}
+    }
+  ],
+  "Fullfillment Id": [],
+  "foid": [],
+  "wo_id": []
+}
