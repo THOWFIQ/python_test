a712528bd88f993668f4a3d964ab817b3ab4d79f
THOWFIQ <sthowfiq@gmail.com>
2025-08-06 00:08:04 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,15 @@
-'Company Name','Contact Type','Special Instruction Id','Special Instruction Type','Shipping City Code','City','First Name','Last Name','State Code','Address Line1','Address Line2','Phone Number','Postal Code'
+[
+  {"value": "Company Name", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Date", "checked": IsPrimary in []},
+  {"value": "Contact Type", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Date", "checked": IsPrimary in []},
+  {"value": "Special Instruction Id", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Date", "checked": IsPrimary in []},
+  {"value": "Special Instruction Type", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Date", "checked": IsPrimary in []},
+  {"value": "Shipping City Code", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Date", "checked": IsPrimary in []},
+  {"value": "City", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Date", "checked": IsPrimary in []},
+  {"value": "First Name", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Date", "checked": IsPrimary in []},
+  {"value": "Last Name", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Date", "checked": IsPrimary in []},
+  {"value": "State Code", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Date", "checked": IsPrimary in []},
+  {"value": "Address Line1", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Date", "checked": IsPrimary in []},
+  {"value": "Address Line2", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Date", "checked": IsPrimary in []},
+  {"value": "Phone Number", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Date", "checked": IsPrimary in []},
+  {"value": "Postal Code", "sortBy": "ascending", "isPrimary": IsPrimary in [], "group": "Date", "checked": IsPrimary in []}
+]
