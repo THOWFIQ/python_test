92c7be26c99f80260a37e8c80e1f7395e65e98e0
THOWFIQ <sthowfiq@gmail.com>
2025-08-25 19:03:57 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,4 +1,23 @@
-final_output = your_dict
-if "logs" in final_output:
-    del final_output["logs"]
-return jsonify(final_output)
+@app.after_request
+def log_response_time(response):
+    if hasattr(g, 'start_time'):
+        duration = time.time() - g.start_time
+        if response.content_type == "application/json":
+            data=json.loads(response.get_data())
+            if type(data) is list:
+                 data[0]["logs"] = {
+                                "urls":g.execution_urls,
+                                "time":g.execution_times
+                            }
+            else:
+                data["logs"] = {
+                                "urls":g.execution_urls,
+                                "time":g.execution_times
+                            }
+            response.set_data(json.dumps(data))
+            response.mimetype="application/json"
+
+            logging.info(f"Response Status: {response.status}")
+            logging.info(f"Total Response time: {duration: .4f} seconds")
+
+    return response
