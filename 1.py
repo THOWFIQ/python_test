5264f29a4ea178e4a08d0a1ed69bc00f72f8f180
THOWFIQ <sthowfiq@gmail.com>
2025-08-25 19:08:04 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,23 +1,3 @@
-@app.after_request
-def log_response_time(response):
-    if hasattr(g, 'start_time'):
-        duration = time.time() - g.start_time
-        if response.content_type == "application/json":
-            data=json.loads(response.get_data())
-            if type(data) is list:
-                 data[0]["logs"] = {
-                                "urls":g.execution_urls,
-                                "time":g.execution_times
-                            }
-            else:
-                data["logs"] = {
-                                "urls":g.execution_urls,
-                                "time":g.execution_times
-                            }
-            response.set_data(json.dumps(data))
-            response.mimetype="application/json"
-
-            logging.info(f"Response Status: {response.status}")
-            logging.info(f"Total Response time: {duration: .4f} seconds")
-
-    return response
+# Skip certain endpoints
+    if request.endpoint in ["healthcheck", "static"]:
+        return response
