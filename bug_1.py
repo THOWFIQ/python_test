4cdbf33de430360ccb02af86e4c9b954b9cffe0d
THOWFIQ <sthowfiq@gmail.com>
2025-09-24 16:50:30 +0530

    Update bug_1.py

--- bug_1.py
+++ bug_1.py
@@ -241,3 +241,512 @@ def newOutputFormat(result_map, format_type=None, region=None, filtersValue=None
 
     except Exception as e:
         return {"error": str(e)}
+
+
+
+
+[
+  {
+    "data": {
+      "getSalesOrderBySoids": {
+        "salesOrders": [
+          {
+            "agreementId": "",
+            "totalPrice": "797",
+            "buid": "10620",
+            "currency": "EUR",
+            "poNumber": "",
+            "dpid": "4382972636",
+            "locationNum": "01",
+            "orderDate": "2025-09-08 17:56:11",
+            "rateUsdTransactional": "0",
+            "orderCreateDate": "2025-09-08 17:56:11",
+            "sourceSystemId": "DSP",
+            "salesOrderId": "4382972636",
+            "region": "EMEA",
+            "orderType": "Standard Order",
+            "address": [
+              {
+                "companyName": "",
+                "cityCode": "",
+                "city": "",
+                "firstName": "",
+                "lastName": "",
+                "fullName": "",
+                "country": "",
+                "stateCode": "",
+                "addressLine1": "",
+                "addressLine2": "",
+                "postalCode": "",
+                "customerNum": "",
+                "customerNameExt": "",
+                "contact": [
+                  {
+                    "contactType": "INSTALLAT"
+                  }
+                ]
+              },
+              {
+                "companyName": "",
+                "cityCode": "",
+                "city": "",
+                "firstName": "",
+                "lastName": "",
+                "fullName": "",
+                "country": "",
+                "stateCode": "",
+                "addressLine1": "",
+                "addressLine2": "",
+                "postalCode": "",
+                "customerNum": "",
+                "customerNameExt": "",
+                "contact": [
+                  {
+                    "contactType": "INSTALLAT"
+                  }
+                ]
+              },
+              {
+                "companyName": "CJR - SGPS, S.A.",
+                "cityCode": "",
+                "city": "GUIMAR\u00c3ES",
+                "firstName": "aenfdyzyd",
+                "lastName": "skxcgjcc",
+                "fullName": "",
+                "country": "PT",
+                "stateCode": "",
+                "addressLine1": "RUA DO LOUREDO\\, 447",
+                "addressLine2": "",
+                "postalCode": "4800-214",
+                "customerNum": "",
+                "customerNameExt": "",
+                "contact": [
+                  {
+                    "contactType": "SOLDTO"
+                  }
+                ]
+              },
+              {
+                "companyName": "",
+                "cityCode": "",
+                "city": "",
+                "firstName": "",
+                "lastName": "",
+                "fullName": "",
+                "country": "",
+                "stateCode": "",
+                "addressLine1": "",
+                "addressLine2": "",
+                "postalCode": "",
+                "customerNum": "",
+                "customerNameExt": "",
+                "contact": [
+                  {
+                    "contactType": "INSTALLAT"
+                  }
+                ]
+              },
+              {
+                "companyName": "",
+                "cityCode": "",
+                "city": "",
+                "firstName": "",
+                "lastName": "",
+                "fullName": "",
+                "country": "",
+                "stateCode": "",
+                "addressLine1": "",
+                "addressLine2": "",
+                "postalCode": "",
+                "customerNum": "",
+                "customerNameExt": "",
+                "contact": [
+                  {
+                    "contactType": "DELIVERY"
+                  }
+                ]
+              },
+              {
+                "companyName": "CJR - SGPS, S.A.",
+                "cityCode": "",
+                "city": "GUIMAR\u00c3ES",
+                "firstName": "tifbhre",
+                "lastName": "olnfwpff",
+                "fullName": "lzplafbhre y mdautfwpff",
+                "country": "PT",
+                "stateCode": "",
+                "addressLine1": "RUA DO LOUREDO\\, N\u00ba 447",
+                "addressLine2": "SELHO S. LOUREN\u00c7O",
+                "postalCode": "4800-214",
+                "customerNum": "",
+                "customerNameExt": "",
+                "contact": [
+                  {
+                    "contactType": "BILLING"
+                  }
+                ]
+              },
+              {
+                "companyName": "",
+                "cityCode": "",
+                "city": "",
+                "firstName": "",
+                "lastName": "",
+                "fullName": "",
+                "country": "",
+                "stateCode": "",
+                "addressLine1": "",
+                "addressLine2": "",
+                "postalCode": "",
+                "customerNum": "",
+                "customerNameExt": "",
+                "contact": [
+                  {
+                    "contactType": "END_USER"
+                  }
+                ]
+              },
+              {
+                "companyName": "CJR - SGPS, S.A.",
+                "cityCode": "",
+                "city": "GUIMAR\u00c3ES",
+                "firstName": "aenfdyzyd",
+                "lastName": "skxcgjcc",
+                "fullName": "aenfdyzyd y skxcgjcc",
+                "country": "PT",
+                "stateCode": "Braga",
+                "addressLine1": "RUA DO LOUREDO\\, N\u00ba 447",
+                "addressLine2": "SELHO S. LOUREN\u00c7O",
+                "postalCode": "4800-214",
+                "customerNum": "D11202988656",
+                "customerNameExt": "",
+                "contact": [
+                  {
+                    "contactType": "SHIPPING"
+                  }
+                ]
+              },
+              {
+                "companyName": "",
+                "cityCode": "",
+                "city": "",
+                "firstName": "",
+                "lastName": "",
+                "fullName": "",
+                "country": "",
+                "stateCode": "",
+                "addressLine1": "",
+                "addressLine2": "",
+                "postalCode": "",
+                "customerNum": "",
+                "customerNameExt": "",
+                "contact": [
+                  {
+                    "contactType": "INSTALLAT"
+                  }
+                ]
+              },
+              {
+                "companyName": "",
+                "cityCode": "",
+                "city": "",
+                "firstName": "",
+                "lastName": "",
+                "fullName": "",
+                "country": "",
+                "stateCode": "",
+                "addressLine1": "",
+                "addressLine2": "",
+                "postalCode": "",
+                "customerNum": "",
+                "customerNameExt": "",
+                "contact": [
+                  {
+                    "contactType": "DELIVERY"
+                  }
+                ]
+              },
+              {
+                "companyName": "",
+                "cityCode": "",
+                "city": "",
+                "firstName": "",
+                "lastName": "",
+                "fullName": "",
+                "country": "",
+                "stateCode": "",
+                "addressLine1": "",
+                "addressLine2": "",
+                "postalCode": "",
+                "customerNum": "",
+                "customerNameExt": "",
+                "contact": [
+                  {
+                    "contactType": "DELIVERY"
+                  }
+                ]
+              },
+              {
+                "companyName": "",
+                "cityCode": "",
+                "city": "",
+                "firstName": "",
+                "lastName": "",
+                "fullName": "",
+                "country": "",
+                "stateCode": "",
+                "addressLine1": "",
+                "addressLine2": "",
+                "postalCode": "",
+                "customerNum": "",
+                "customerNameExt": "",
+                "contact": [
+                  {
+                    "contactType": "DELIVERY"
+                  }
+                ]
+              }
+            ],
+            "fulfillments": [
+              {
+                "deliveryCity": "",
+                "mergeType": "ATO-DIRECT",
+                "paymentTerm": "Prepaid",
+                "shipCode": "IY",
+                "systemQty": "1",
+                "shipByDate": "",
+                "mustArriveByDate": "",
+                "manifestDate": "",
+                "revisedDeliveryDate": "1900-01-01 00:00:00",
+                "oicId": "bb736b2b-0ee9-45d0-a5f8-dcf4dbd2c7bc",
+                "fulfillmentId": "5240818296",
+                "address": [
+                  {
+                    "postalCode": "",
+                    "taxRegstrnNum": "",
+                    "contact": [
+                      {
+                        "contactType": "INSTALLAT"
+                      }
+                    ],
+                    "phone": []
+                  },
+                  {
+                    "postalCode": "",
+                    "taxRegstrnNum": "",
+                    "contact": [
+                      {
+                        "contactType": "INSTALLAT"
+                      }
+                    ],
+                    "phone": []
+                  },
+                  {
+                    "postalCode": "",
+                    "taxRegstrnNum": "",
+                    "contact": [
+                      {
+                        "contactType": "INSTALLAT"
+                      }
+                    ],
+                    "phone": []
+                  },
+                  {
+                    "postalCode": "",
+                    "taxRegstrnNum": "",
+                    "contact": [
+                      {
+                        "contactType": "DELIVERY"
+                      }
+                    ],
+                    "phone": []
+                  },
+                  {
+                    "postalCode": "",
+                    "taxRegstrnNum": "",
+                    "contact": [
+                      {
+                        "contactType": "END_USER"
+                      }
+                    ],
+                    "phone": []
+                  },
+                  {
+                    "postalCode": "4800-214",
+                    "taxRegstrnNum": "",
+                    "contact": [
+                      {
+                        "contactType": "SHIPPING"
+                      }
+                    ],
+                    "phone": []
+                  },
+                  {
+                    "postalCode": "",
+                    "taxRegstrnNum": "",
+                    "contact": [
+                      {
+                        "contactType": "INSTALLAT"
+                      }
+                    ],
+                    "phone": []
+                  },
+                  {
+                    "postalCode": "",
+                    "taxRegstrnNum": "",
+                    "contact": [
+                      {
+                        "contactType": "DELIVERY"
+                      }
+                    ],
+                    "phone": []
+                  },
+                  {
+                    "postalCode": "",
+                    "taxRegstrnNum": "",
+                    "contact": [
+                      {
+                        "contactType": "DELIVERY"
+                      }
+                    ],
+                    "phone": []
+                  },
+                  {
+                    "postalCode": "",
+                    "taxRegstrnNum": "",
+                    "contact": [
+                      {
+                        "contactType": "DELIVERY"
+                      }
+                    ],
+                    "phone": []
+                  }
+                ],
+                "soStatus": [
+                  {
+                    "sourceSystemStsCode": "PP",
+                    "fulfillmentStsCode": "PENDFULFILL",
+                    "statusDate": "2025-09-22 05:30:56.173993"
+                  }
+                ],
+                "salesOrderLines": [
+                  {
+                    "soLineQty": "1",
+                    "lob": "",
+                    "siNumber": "",
+                    "soLineNum": "31",
+                    "facility": "",
+                    "specialInstructions": []
+                  },
+                  {
+                    "soLineQty": "1",
+                    "lob": "Laptop",
+                    "siNumber": "",
+                    "soLineNum": "1",
+                    "facility": "CET",
+                    "specialInstructions": []
+                  },
+                  {
+                    "soLineQty": "1",
+                    "lob": "Software",
+                    "siNumber": "",
+                    "soLineNum": "29",
+                    "facility": "",
+                    "specialInstructions": []
+                  },
+                  {
+                    "soLineQty": "1",
+                    "lob": "Software",
+                    "siNumber": "",
+                    "soLineNum": "30",
+                    "facility": "",
+                    "specialInstructions": []
+                  }
+                ],
+                "fulfillmentOrder": [
+                  {
+                    "foId": "7375763532200185857"
+                  }
+                ]
+              }
+            ],
+            "salesRep": [
+              {
+                "salesRepName": ""
+              }
+            ]
+          }
+        ]
+      }
+    }
+  },
+  {
+    "data": {
+      "getWorkOrderByWoIds": [
+        {
+          "channel": "CM",
+          "vendorSiteId": "CET",
+          "dellBlanketPoNum": "",
+          "woLines": [
+            {
+              "ismultipack": "",
+              "woLineType": "MAKE"
+            }
+          ],
+          "woType": "MAKE",
+          "woId": "69748",
+          "woStatusList": [
+            {
+              "channelStatusCode": "1000"
+            }
+          ],
+          "woShipInstr": [],
+          "shipMode": "AR",
+          "shipToFacility": "CUST"
+        },
+        {
+          "channel": "CM",
+          "vendorSiteId": "CET",
+          "dellBlanketPoNum": "",
+          "woLines": [
+            {
+              "ismultipack": "",
+              "woLineType": "MAKE"
+            }
+          ],
+          "woType": "MAKE",
+          "woId": "69759",
+          "woStatusList": [
+            {
+              "channelStatusCode": "1000"
+            }
+          ],
+          "woShipInstr": [],
+          "shipMode": "AR",
+          "shipToFacility": "CUST"
+        },
+        {
+          "channel": "CM",
+          "vendorSiteId": "CET",
+          "dellBlanketPoNum": "",
+          "woLines": [
+            {
+              "ismultipack": "",
+              "woLineType": "MAKE"
+            }
+          ],
+          "woType": "MAKE",
+          "woId": "69770",
+          "woStatusList": [
+            {
+              "channelStatusCode": "1000"
+            }
+          ],
+          "woShipInstr": [],
+          "shipMode": "AR",
+          "shipToFacility": "CUST"
+        }
+      ]
+    }
+  }
+]
